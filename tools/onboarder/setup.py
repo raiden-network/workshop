bc34094b8eaf60db099b5e93084c1f6faecec171
Ulrich Petri <ulo@ulo.pe>
2019-06-03 17:49:03 +0200

    Update requirements, fix Raiden signature change

--- tools/onboarder/setup.py
+++ tools/onboarder/setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'click==6.7',
         'eth-keyfile==0.5.1',
-        'eth-utils==1.2.1',
+        'eth-utils==1.3.0',
         'requests==2.20.0',
     ],
     entry_points={
